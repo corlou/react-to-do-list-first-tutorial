ca34d84570fdf95d73214ce098f35fbbe1107d1f
Corina Pelichowski <corina@danitt.com>
2019-06-27 22:42:03 +1000

    adding line-through decoration without ternary operator

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
+  getStyle = () => {
+     if(this.props.todo.completed) {
+      return {
+        textDecoration: 'line-through'
+        }
+     } else {
+       return {
+         textDecoration: 'none'
+       }
+     }
+  }
   render() {
     return (
-      //In-line styling needs double curly braces and camel case, no hyphens
-      <div style={{backgroundColor:'#f4f4f4'}}>
+      
+      <div style={this.getStyle()}>
         <p>{ this.props.todo.title}</p>
       </div>
     )
@@ -16,5 +27,11 @@ export class Todoitem extends Component {
 Todoitem.propTypes = {
   todo: PropTypes.object.isRequired
 }
-
+//In-line styling needs double curly braces and camel case, no hyphens
+/*If I need more styling options, to use it here
+const itemStyle = {
+  backgroundColor:'#f4f4f4',
+  fontSize: '18px' 
+}
+*/
 export default Todoitem
